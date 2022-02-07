023e781ac102206097130e814250fcfcea4fcc63
ahmedmohab96 <ahmdmhabhbyb@gmail.com>
2022-02-07 18:41:31 +0200

    https://github.com/ahmedmohab96/Clima_app.git

---
+++ lib/screens/city_screen.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class CityScreen extends StatefulWidget {
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  late String cityName;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/city_background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topLeft,
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    size: 50.0,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                child: TextField(
+                  onChanged: (value) {
+                    cityName = value;
+                  },
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                  decoration: kInputField,
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.pop(context, cityName);
+                },
+                child: Text(
+                  'Get Weather',
+                  style: kButtonTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
